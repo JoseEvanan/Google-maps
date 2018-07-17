cf9b355feab398eff09402eea8bee7545b58804a
JoseEvanan <jose.evanan@gmail.com>
2018-07-17 09:07:48 -0500

    add script export image

---
+++ export_maps.py
@@ -0,0 +1,16 @@
+import requests
+payload = {}
+
+payload["center"] = "Lima"
+payload["zoom"] = "10"
+payload["size"] = "600x300"
+payload["maptype"] = "roadmap"
+payload["markers"] = "color:blue|label:S|-12.0548898,-77.0792565"
+payload["key"] = "AIzaSyDgSz4IaDDexEsAA8gOozP-na1sCDregn0"
+
+url = "https://maps.googleapis.com/maps/api/staticmap"
+
+
+img = open('google-maps.png','wb')
+img.write(requests.get(url, params=payload).content)
+img.close()
