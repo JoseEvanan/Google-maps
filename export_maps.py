01fb727aebc91de36c55c7de9ff1b07f638f923e
JoseEvanan <jose.evanan@gmail.com>
2018-07-17 09:52:26 -0500

    add script google maps

--- export_maps.py
+++ export_maps.py
@@ -6,7 +6,7 @@ payload["zoom"] = "10"
 payload["size"] = "600x300"
 payload["maptype"] = "roadmap"
 payload["markers"] = "color:blue|label:S|-12.0548898,-77.0792565"
-payload["key"] = "AIzaSyDgSz4IaDDexEsAA8gOozP-na1sCDregn0"
+payload["key"] = "KEY_API_MAP"
 
 url = "https://maps.googleapis.com/maps/api/staticmap"
 
